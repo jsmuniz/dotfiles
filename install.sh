3854c800de8bbfe24ddb6b6fc7e0949f941b8222
Jonathan Muniz <jonathanmuniz@pop-os.localdomain>
2021-03-13 02:36:06 -0300

    Adicionando aplicações

--- install.sh
+++ install.sh
@@ -3,28 +3,47 @@
 #Adding ppas
 add-apt-repository ppa:atareao/telegram -y
 
+
 #Updating apt
 apt update
 
 #Installing packages
 
 #Applications
+
+#General
 apt install telegram -y
 
+dpkg --status teams &> /dev/null
+if ! [ $? -eq 0 ]; then
+    sudo -u jonathanmuniz wget "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=linux&arch=x64&download=true&linuxArchiveType=deb" -O teams.deb
+    apt install ./teams.deb
+    rm -rf teams.deb
+    sudo apt-get install -y $1
+fi
+
+
+#Programming
+apt install code -y
+sudo -u jonathanmuniz flatpak install flathub com.jetbrains.Rider -y
+sudo apt-get install -y gitk 
+
+git clone https://github.com/dracula/gitk.git
+mkdir -p /home/jonathanmuniz/.config/git
+cp gitk/gitk /home/jonathanmuniz/.config/git/
+rm -rf gitk
 
 #Command Line Tools
 apt install zsh -y
 apt install guake -y
 
-#Guake como terminal default
-gsettings set org.gnome.desktop.default-applications.terminal exec 'guake --new-tab'
-gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
-
-
 #Instalando PowerLevel10k
 if [ ! -d "/home/jonathanmuniz/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/jonathanmuniz/.oh-my-zsh/custom/themes/powerlevel10k
 fi
+
+#Instalando zinit
+sudo -u jonathanmuniz sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 
 
--- zshrc
+++ zshrc
@@ -109,3 +109,22 @@ alias ohmyzsh="gedit ~/.oh-my-zsh"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+### Added by Zinit's installer
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+### End of Zinit's installer chunk
+
+zinit light zdharma/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+zinit light supercrabtree/k

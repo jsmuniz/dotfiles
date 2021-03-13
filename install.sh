f86dddd48670602b81fc01ddfd553e24902b7a5c
Jonathan Muniz <jonathanmuniz@pop-os.localdomain>
2021-03-13 11:37:45 -0300

    Adicionando Docker

--- install.sh
+++ install.sh
@@ -41,9 +41,37 @@ apt install -y apt-transport-https
 apt update
 apt install dotnet-sdk-5.0 -y
 
+#Docker
+
+dpkg --status docker-ce &> /dev/null
+if ! [ $? -eq 0 ]; then
+	apt install ca-certificates -y
+	apt install curl -y
+	apt install gnupg -y
+	apt install lsb-release -y
+
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+	echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+	apt update
+	apt install -y docker-ce docker-ce-cli containerd.io
+	
+	
+	groupadd docker
+	usermod -aG docker $USER
+	newgrp docker
+fi
+
+
 #Command Line Tools
 apt install zsh -y
 apt install guake -y
+apt install vim -y
+git config --global core.editor "vim"
 
 #Instalando PowerLevel10k
 if [ ! -d "/home/jonathanmuniz/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
